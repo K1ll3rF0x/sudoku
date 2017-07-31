e36b2a815f8d93734d56d9ae15b6a9373b7bbaa5
Toni Dahl <toni.dahl@solita.fi>
2017-07-31 09:58:17 +0300

    Add solutions for exercises 1-7.

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -3,26 +3,41 @@
 
 (def board identity)
 
+(def all-values #{1 2 3 4 5 6 7 8 9})
+
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (pos? (value-at board coord)))
 
-(defn row-values [board coord]
-  nil)
+(defn row-values [board [row col]]
+  (set (get board row)))
 
-(defn col-values [board coord]
-  nil)
+(defn col-values [board [row col]]
+  (set (map #(get %1 col) board)))
 
 (defn coord-pairs [coords]
-  nil)
+  (for [row coords
+        col coords]
+     [row col]))
+
+(defn block-coord [[row col]]
+  [(* 3 (int (/ row 3))) (* 3 (int (/ col 3)))])
 
 (defn block-values [board coord]
-  nil)
+  (let [[top-y top-x] (block-coord coord)]
+    (set (for [y (range 3)
+               x (range 3)]
+           (value-at board [(+ y top-y) (+ x top-x)])))))
 
 (defn valid-values-for [board coord]
-  nil)
+  (if (has-value? board coord)
+    #{}
+    (set/difference all-values
+                  (row-values board coord)
+                  (col-values board coord)
+                  (block-values board coord))))
 
 (defn filled? [board]
   nil)
